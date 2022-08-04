2d680756db094a1732b0adec5deb548adc71562e
Ilf Ali Momin <109297627+Mominnest@users.noreply.github.com>
2022-08-03 20:11:22 -1200

    Update 02strings.js

---
+++ 05Advance/02strings.js
@@ -0,0 +1,8 @@
+var name = `ali`;
+var fullName = `${name} choudhary`;
+
+console.log(fullName);
+console.log(fullName.charAt(0));
+console.log(fullName.endsWith("?"));
+console.log(fullName.includes("ali"));
+console.log(fullName.toUpperCase());
